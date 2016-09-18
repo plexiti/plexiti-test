061397003bc4bf493627a89bdee5f83f554d6f6b
Martin Schimak <martin.schimak@plexiti.com>
2016-09-18 19:57:35 +0200

    publish: feat(lang-redirect): add script to redirect to preferred language
    generated from commit 1aa20596bfb6b4a6fa9fc1e50e44895fd1fb9919

--- en/js/lang.js
+++ en/js/lang.js
@@ -19,3 +19,25 @@ function getCookie(cname) {
     }
     return "";
 }
+
+function redirect2language(requestedLang) {
+    var hostName = document.referrer.split('/')[2];
+    var langCode = getCookie('lang');
+    if (hostName && hostName !== 'plexiti.com' && langCode && langCode !== requestedLang) {
+        var path = window.location.pathname;
+        path = '/' + langCode + path.substr(3, path.length - 1);
+        window.location.pathname = path;
+    }
+}
+
+function redirect2blog() {
+    var langCode = getCookie('lang') || navigator.language || navigator.systemLanguage;
+    var lang = langCode.toLowerCase();
+    lang = lang.substr(0,2);
+    var dest = window.location.href;
+    if (lang=="de") {
+        window.location.pathname = window.location.pathname + "de/blog/"
+    } else {
+        window.location.pathname = window.location.pathname + "en/blog/"
+    }
+}
