de869aebb071a885f513f4989900656a06a5fe21
Martin Schimak <martin.schimak@plexiti.com>
2016-09-21 19:11:20 +0200

    publish: feat(lang-redirect): add script to redirect to preferred language
    generated from commit af68c08bc0866a8584155b5c972eb1d90c0f2ac3

--- en/js/lang.js
+++ en/js/lang.js
@@ -19,3 +19,33 @@ function getCookie(cname) {
     }
     return "";
 }
+
+function redirect2language(contextPath) {
+    var referrer = document.referrer;
+    console.log("referrer: " + referrer);
+    var contextHost = contextPath.substr(0, contextPath.length - 4);
+    console.log("contextHost: " + contextHost);
+    if (referrer && !referrer.startsWith(contextHost)) {
+        var userLang = getCookie('lang');
+        console.log("userLang: " + userLang);
+        var contextLang = contextPath.substr(contextPath.length - 3, contextPath.length - 2);
+        console.log("contextLang: " + contextLang);
+        if (contextLang !== userLang) {
+            var path = window.location.pathname.replace('/' + contextLang + '/', '/' + userLang + '/');
+            console.log("path: " + path);
+            window.location.pathname = path;
+        }
+    }
+}
+
+function redirect2blog() {
+    var langCode = getCookie('lang') || navigator.language || navigator.systemLanguage;
+    var lang = langCode.toLowerCase();
+    lang = lang.substr(0,2);
+    var dest = window.location.href;
+    if (lang=="de") {
+        window.location.pathname = window.location.pathname + "de/blog/"
+    } else {
+        window.location.pathname = window.location.pathname + "en/blog/"
+    }
+}
